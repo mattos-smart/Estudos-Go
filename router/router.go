b0fec2c26dbedc1238bb4561371a994d42b08d07
mattos-smart <mattos@smartsis.com>
2026-05-05 14:16:34 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -4,14 +4,12 @@ import "github.com/gin-gonic/gin" //se nao definiu nada, pega o ultimo nome do p
 
 // Quando a func iniciar com letra Maiuscula, ele está sendo exportado de maneira automatica
 func Initialize() {
-	// Inicializa o Router utilizando as configurações defaults do Gin
+	// Initialize Router
 	router := gin.Default()
-	// Definindo uma rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// Estamos rodando a API
+
+	// Initialize routes
+	initializeRoutes(router)
+
+	// Run the server
 	router.Run(":8080")
 }
